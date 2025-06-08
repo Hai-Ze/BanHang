e153899ddf8b6fff8ca4035608a02c579f0610e8
Zes <Zes@HAI>
2025-06-08 09:58:29 +0700

    view home cho admin

--- BanHang/Areas/Admin/Controllers/HomeController.cs
+++ BanHang/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BanHang.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace BanHang.Areas.Admin.Controllers
 {
@@ -7,14 +10,27 @@ namespace BanHang.Areas.Admin.Controllers
     [Authorize(Roles = SD.Role_Admin)]  
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context; // Khai báo biến trường
+
+        // Constructor để inject DbContext
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context ?? throw new ArgumentNullException(nameof(context)); // Kiểm tra null
         }
 
         public IActionResult Dashboard()
         {
             return View("Index");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            var userCount = await _context.Users.CountAsync(); // Số người dùng từ AspNetUsers
+            var categoryCount = await _context.Categories.CountAsync(); 
+
+            ViewBag.UserCount = userCount;
+            ViewBag.CategoryCount = categoryCount;
+            return View(products);
         }
     }
 }
